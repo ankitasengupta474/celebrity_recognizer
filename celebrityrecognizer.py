5ec465a5d56e82ca36ffd1c2e5aeff0e575e0582
ankitasengupta474 <121786008+ankitasengupta474@users.noreply.github.com>
2024-12-25 20:29:35 +0530

    Add files via upload

---
+++ celebrityrecognizer.py
@@ -0,0 +1,75 @@
+### celebrity recognizer
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load Google Gemini Pro Vision API And get response
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-1.5-flash')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="celebrity recognizer")
+
+st.header("who's that celebrity?")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about this celebrity")
+
+input_prompt="""
+You are an expert in the media world and you know every minute detail about any given celebrity like what they do , what they are known 
+for and when they were born from the picture provided by the user in the given format 
+
+               1. tell me what they are famous for
+               2. when were they born
+               3. biodata
+               4. are they alive if not give death date
+               5. most famous work
+               6. scandals if any
+               7. active or not
+               
+               ----
+               ----
+
+
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
